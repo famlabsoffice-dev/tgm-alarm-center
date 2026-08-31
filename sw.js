722b4ab458a1b715b44697e9e6996b212070ba18
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-31 18:34:11 +0200

    refine: version offline app shell cache

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-const CACHE='tgm-alarm-center-v1';
+const CACHE='tgm-alarm-center-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});return response}).catch(()=>cached)))});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});
+    return response;
+  }).catch(()=>cached)));
+});
